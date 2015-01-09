1eabbdbbae47269dddb10abaf8df5cf5e0ac63e2
Jan-Philip Loos <maxdaten@gmail.com>
2015-01-09 07:37:34 +0100

    added explicit Image2D (MipmapChain DynamicImage) instance

--- src/Quine/MipmapChain.hs
+++ src/Quine/MipmapChain.hs
@@ -1,6 +1,7 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE LambdaCase       #-}
 {-# LANGUAGE FlexibleContexts #-}
 --------------------------------------------------------------------
 -- |
@@ -27,7 +28,9 @@ module Quine.MipmapChain
 
 import Prelude hiding (zipWith,tail,head,length,sequence_)
 import Codec.Picture
+import Codec.Picture.Types (dynamicMap)
 import Data.Foldable (sequence_)
+import Data.Proxy
 import Data.List.NonEmpty
 import Graphics.GL.Ext.ARB.TextureStorage
 import Quine.Image
@@ -69,4 +72,24 @@ instance (ImageFormat a, Image2D (Image a)) => Image2D (MipmapChain (Image a)) w
   store chain t = do
     let base@(Image w h _) = mipMapBase chain
     glTexStorage2D t (fromIntegral $ maxMipMapLevel chain + 1) (internalFormat base) (fromIntegral w) (fromIntegral h)
-    upload chain t 0
+
+instance Image2D (MipmapChain DynamicImage) where
+  upload chain t _l = sequence_ $ zipWith (\img l -> upload img t l) chain (mkMipmapChain 0 [1..])
+  store chain t = 
+    let base = (mipMapBase chain) in glTexStorage2D t (fromIntegral $ maxMipMapLevel chain + 1) fmt (fromIntegral $ dynamicMap imageWidth base) (fromIntegral $ dynamicMap imageHeight base)  
+    where 
+    fmt = case (mipMapBase chain) of
+      (ImageY8 i)      -> internalFormat i
+      (ImageY16 i)     -> internalFormat i
+      (ImageYF i)      -> internalFormat i
+      (ImageYA8 i)     -> internalFormat i
+      (ImageYA16 i)    -> internalFormat i
+      (ImageRGB8 i)    -> internalFormat i
+      (ImageRGB16 i)   -> internalFormat i
+      (ImageRGBF i)    -> internalFormat i
+      (ImageRGBA8 i)   -> internalFormat i
+      (ImageRGBA16 i)  -> internalFormat i
+      -- the following formats are converted by the Image2D DynamicImage instance
+      (ImageYCbCr8 _)  -> internalFormat (Proxy :: Proxy PixelRGB8)
+      (ImageCMYK8 _)   -> internalFormat (Proxy :: Proxy PixelRGB8)
+      (ImageCMYK16 _)  -> internalFormat (Proxy :: Proxy PixelRGB16)
